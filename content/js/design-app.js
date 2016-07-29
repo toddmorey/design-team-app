e3c7ec7ea202b27c4518f3267177c5fcacb74c91
Wes Wilson <wes@iamweswilson.com>
2016-07-29 00:04:03 -0500

    back to top button

--- content/js/design-app.js
+++ content/js/design-app.js
@@ -5,3 +5,17 @@ $('a').click(function(){
     }, 500);
     return false;
 });
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 1
+    }, 500);
+});
